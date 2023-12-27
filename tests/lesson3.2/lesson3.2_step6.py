78e1020f3e3aea22938ea17e1f4ac7f3292d5dae
DavydovDY <denis.davydov@vitech.team>
2023-12-27 13:53:12 +0300

    Проверка ожидаемого результата

--- tests/lesson3.2/lesson3.2_step6.py
+++ tests/lesson3.2/lesson3.2_step6.py
@@ -1,9 +1,3 @@
-import math
-import time
-
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from selenium import webdriver
 
 try:
