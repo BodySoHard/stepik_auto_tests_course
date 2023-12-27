a20cd5a9e79af66a7a90da85572c2a0562cc5756
DavydovDY <denis.davydov@vitech.team>
2023-12-27 14:19:53 +0300

    Проверка ожидаемого результата

--- tests/lesson3.2/lesson3.2_step6.py
+++ tests/lesson3.2/lesson3.2_step6.py
@@ -6,6 +6,28 @@ try:
     assert abs(-42) == 42
     # есть ошибка
     # assert abs(-42) == -42
+    # Форматирование строк с помощью str.format
+    print("Let's count together: {}, then goes {}, and then {}".format("one", "two", "three"))
+    # Форматирование строк с помощью f-strings
+    str1 = "one"
+    str2 = "two"
+    str3 = "three"
+    print(f"Let's count together: {str1}, then goes {str2}, and then {str3}")
+
+    #
+    actual_result = "abrakadabra"
+    print(f"Wrong text, got {actual_result}, something wrong")
+
+    # составные сообщения об ошибках и поиск подстроки
+    s = 'My Name is Julia'
+
+    if 'Name' in s:
+        print('Substring found')
+
+    index = s.find('Name')
+    if index != -1:
+        print(f'Substring found at index {index}')
+
     # есть ошибка с описанием
     assert abs(-42) == -42, "Значение не положительное"
 
